4edde14fd15761cb04ba7d5985730b3631a2c15c
Thuyan Duong <thuyan.duong@flatironschool.com>
2018-10-21 20:32:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,10 @@
 # code your #position_taken? method here!
 def position_taken(board, index)
-  taken = nil
+  taken
   if board[index] == " "
     taken = false
   else
     taken = true
+  end
   taken
 end
